dd3786ce0589a8bddd92080364669438a79097ca
Aniket Kumar <aa6443@srmist.edu.in>
2024-04-23 11:33:58 +0530

    react basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Chai from './chai.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Chai />
+      <h2>This is my project using </h2>
+      <h2>React</h2>
+      </>
     </div>
   );
 }
---
+++ src/chai.js
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <h3>Chai is ready </h3>
+    )
+};
+
+//exporting function is components here 
+
+
+export default Chai 
